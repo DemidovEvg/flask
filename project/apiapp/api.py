87b0f8dc2d0d4e1f793dfe986829ee31ff0423f9
DemidovEV <demidov.evg.v@gmail.com>
2023-01-31 23:40:18 +0300

    dz10

--- project/apiapp/api.py
+++ project/apiapp/api.py
@@ -1,5 +1,7 @@
 from flask_combo_jsonapi import Api
 from combojsonapi.spec import ApiSpecPlugin
+from combojsonapi.event import EventPlugin
+from combojsonapi.permission import PermissionPlugin
 from . import users_api
 from . import truck_api
 from . import place_api
@@ -19,11 +21,15 @@ def create_api_spec_plugin(app):
 
 
 def init_api(app):
+    event_plugin = EventPlugin()
     api_spec_plugin = create_api_spec_plugin(app)
+    permission_plugin = PermissionPlugin()
     api = Api(
         app,
         plugins=[
-            api_spec_plugin
+            event_plugin,
+            api_spec_plugin,
+            permission_plugin
         ]
     )
     users_api.register(api)
--- project/shemas_mapping.py
+++ project/shemas_mapping.py
@@ -1,6 +1,9 @@
 
 from sqlalchemy.orm import Session
+import sqlalchemy as sa
+from flask import abort
 from flask_combo_jsonapi import ResourceDetail, ResourceList
+from combojsonapi.event.resource import EventsResource
 from project.models import User, Truck, Place, Product, Trip
 from project.database import db
 from project.shemas import (
@@ -10,86 +13,93 @@ from project.shemas import (
     PlaceSchemaCombo,
     ProductSchemaCombo
 )
+from project.permissions import IsAuthenticatedPermission
 
 
 session: Session = db.session
 
 
+base_data_layer = {
+    'session': session,
+    'permission_get': [IsAuthenticatedPermission],
+    'permission_patch': [IsAuthenticatedPermission],
+    'permission_post': [IsAuthenticatedPermission],
+    'permission_delete': [IsAuthenticatedPermission]
+}
+
+
 class UserList(ResourceList):
+    data_layer = dict(model=User)
+    data_layer.update(base_data_layer)
     schema = UserSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': User
-    }
 
 
 class UserDetail(ResourceDetail):
     schema = UserSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': User
-    }
+    data_layer = dict(model=User)
+    data_layer.update(base_data_layer)
 
 
 class TruckList(ResourceList):
     schema = TruckSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Truck
-    }
+    data_layer = dict(model=Truck)
+    data_layer.update(base_data_layer)
 
 
 class TruckDetail(ResourceDetail):
     schema = TruckSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Truck
-    }
+    data_layer = dict(model=Truck)
+    data_layer.update(base_data_layer)
 
 
 class TripList(ResourceList):
     schema = TripSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Trip
-    }
+    data_layer = dict(model=Trip)
+    data_layer.update(base_data_layer)
 
 
 class TripDetail(ResourceDetail):
     schema = TripSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Trip
-    }
+    data_layer = dict(model=Trip)
+    data_layer.update(base_data_layer)
+
+
+class ProductListEvents(EventsResource):
+    def event_get_most_popular_product(self):
+        products = (
+            session.query(Product,
+                          sa.func.count(Trip.id).label('trips_count'))
+            .join(Product.trips)
+            .group_by(Product.id)
+            .order_by(sa.desc(sa.func.count(Trip.id).label('trips_count')))
+            .all()
+        )
+        if products:
+            return {'most_popular_product': products[0][0].name}
+        else:
+            raise abort(404)
 
 
 class ProductList(ResourceList):
+    events = ProductListEvents
     schema = ProductSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Product
-    }
+    data_layer = dict(model=Product)
+    data_layer.update(base_data_layer)
 
 
 class ProductDetail(ResourceDetail):
     schema = ProductSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Product
-    }
+    data_layer = dict(model=Product)
+    data_layer.update(base_data_layer)
 
 
 class PlaceList(ResourceList):
     schema = PlaceSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Place
-    }
+    data_layer = dict(model=Place)
+    data_layer.update(base_data_layer)
 
 
 class PlaceDetail(ResourceDetail):
     schema = PlaceSchemaCombo
-    data_layer = {
-        'session': session,
-        'model': Place
-    }
+    data_layer = dict(model=Place)
+    data_layer.update(base_data_layer)
